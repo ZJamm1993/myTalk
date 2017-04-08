7917ae1d07618d308275e85822113bc77d0f03ee
jam <jam@jamdemacbook-pro.local>
2017-04-08 18:50:19 +0800

    Initial Commit

---
+++ myTalk/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  myTalk
+//
+//  Created by jam on 17/4/8.
+//  Copyright © 2017年 jam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
